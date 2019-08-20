d5e29eb996dd3fa1b58052aea02c5bb4a9a7f3c9
robomantis19 <freefavors@gmail.com>
2019-08-20 16:33:53 -0400

    array-methods.js done

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -58,28 +58,64 @@ const runners = [
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs. Combine both the first and last names and populate a new array called `fullNames`. This array will contain just strings.
 let fullNames = [];
+runners.forEach(function(x){
+  return fullNames.push(`${x.first_name}, ${x.last_name}`)
+})
+
 console.log(fullNames);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runners' first names in uppercase because the director BECAME DRUNK WITH POWER. Populate an array called `firstNamesAllCaps`. This array will contain just strings.
 let firstNamesAllCaps = [];
+runners.map(function(x){
+  return firstNamesAllCaps.push(x.first_name.toUpperCase());
+})
 console.log(firstNamesAllCaps);
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue. We need a filtered version of the runners array, containing only those runners with large sized shirts so they can choose a different size. This will be an array of objects.
 let runnersLargeSizeShirt = [];
+runners.filter( (x) =>{
+  if(x.shirt_size === "L"){
+    return runnersLargeSizeShirt.push(x);
+  };
+})
 console.log(runnersLargeSizeShirt);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations and save the total into a ticketPriceTotal variable.
 let ticketPriceTotal = 0;
+
+ticketPriceTotal = runners.reduce(function(acc, item){
+  return acc + item.donation;
+},0);
+
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
-
+// filter all emails.
+const AllEmails = []
+runners.filter((x)=>{
+  return AllEmails.push(x.email);
+})
+console.log(AllEmails)
 // Problem 2
+//filter for last name so we cant sort it alphabetically.
+const InOrder = []
+runners.filter(function(x){
+  return InOrder.push(x.last_name);
+})
+console.log(InOrder.sort());
 
 // Problem 3
+// find company Toni is with.
+const InOrder2 = []
+runners.filter(function(x){
+  if(x.first_name == "Toni"){
+    return InOrder2.push(`${x.company_name}`);
+  };
+})
+console.log(InOrder2);
